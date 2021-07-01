7be17cfdf56eea9ed77511f7f959ba4d47cc3304
Max <e13lun4@yandex.ru>
2021-07-01 23:33:01 +0300

    peredelal

--- backend/config/server.js
+++ backend/config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'e833c98d0fa5d9175a255cf4bf5a4130'),
+      secret: env('ADMIN_JWT_SECRET', 'd6cbe4c30a5b95c2f14dad222d66b1bb'),
     },
   },
 });
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'f4857bf9-d6a2-41a3-b364-cb4758fe3993'
-};
