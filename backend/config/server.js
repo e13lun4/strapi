e1e9a75f460e6892fac8a6cee41c017370f47661
Max <e13lun4@yandex.ru>
2021-08-05 13:21:46 +0300

    antonu

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'd6cbe4c30a5b95c2f14dad222d66b1bb'),
+      secret: env("ADMIN_JWT_SECRET", "d6cbe4c30a5b95c2f14dad222d66b1bb"),
     },
   },
 });
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0d167d77-5fdf-4760-a39f-d2e008ff509b'
+};
